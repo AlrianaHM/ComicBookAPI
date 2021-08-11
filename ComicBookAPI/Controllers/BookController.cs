9077ab90f99423610c73089494e5ddef99d2d3aa
alriana <alriana.dev@gmail.com>
2021-08-11 20:09:10 +0700

    add database model

---
+++ ComicBookAPI/Controllers/BookController.cs
@@ -0,0 +1,23 @@
+﻿using DatabaseService.DatabaseModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ComicBookAPI.Controllers
+{
+    public class BookController : ApiController
+    {
+
+        public IEnumerable<Book> Get()
+        {
+            using (ComicBookEntities entities = new ComicBookEntities())
+            {
+                return entities.Books.ToList();
+            }
+        }
+
+    }
+}
